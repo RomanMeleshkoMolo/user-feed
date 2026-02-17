43f87956fc395e682404dbc35ababb7d175c9efd
Roman <roman@macbook.local>
2026-02-17 23:31:30 +0200

    Added fileter items

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,14 @@ const userSchema = new mongoose.Schema({
   name: { type: String, index: true, unique: true, sparse: true },
   email: { type: String, index: true, unique: true, sparse: true },
   interests: { type: [String], default: [] },
+  education: { type: String, default: '' },
+  lookingFor: {
+    id: { type: String },
+    title: { type: String },
+    icon: { type: String, default: '' },
+  },
+  about: { type: String, default: '' },
+  work: { type: String, default: '' },
   googleId: { type: String, index: true, unique: true, sparse: true },
   age: { type: Number },
   userBirthday: { type: String },
@@ -15,7 +23,18 @@ const userSchema = new mongoose.Schema({
   userPhotoUrls: [{ type: String }],
   tabIconUserProfile: { type: String },
   userLocation: { type: String, index: true },
-  userSex: { type: String, enum: ['heterosexual', 'gay', 'lesbian', 'bisexual', 'asexual'] }
+  userSex: { type: String, enum: ['heterosexual', 'gay', 'lesbian', 'bisexual', 'asexual'] },
+  zodiac: { type: String, default: '' },
+  languages: { type: [String], default: [] },
+  children: { type: String, default: '' },
+  pets: { type: [String], default: [] },
+  smoking: { type: String, default: '' },
+  alcohol: { type: String, default: '' },
+  relationship: { type: String, default: '' },
+
+  // Онлайн статус
+  isOnline: { type: Boolean, default: false },
+  lastSeen: { type: Date, default: null },
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
